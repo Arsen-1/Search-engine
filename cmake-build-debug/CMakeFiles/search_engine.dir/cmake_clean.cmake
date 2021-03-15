11c2dbec0687718190f7e75f335a33202c82d748
Arsen-1 <69711993+arsen-1@users.noreply.github.com>
2021-03-15 15:14:35 +0000

    Add

---
+++ cmake-build-debug/CMakeFiles/search_engine.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_engine.dir/main.cpp.obj"
+  "search_engine.exe"
+  "search_engine.exe.manifest"
+  "search_engine.lib"
+  "search_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
